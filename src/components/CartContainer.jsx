18d526473643a03a74d18f4962adb1754b21c92a
kianibound <kianibound@gmail.com>
2023-04-05 13:13:36 +0330

    added full functionality

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 const CartContainer = () => {
   const dispatch = useDispatch();
   const { amount, total, cartItems } = useSelector((store) => store.cart);
@@ -31,12 +31,12 @@ const CartContainer = () => {
           <hr />
           <div className="cart-total">
             <h4>
-              total<span>{total}</span>
+              total<span>{total.toFixed(2)}</span>
             </h4>
           </div>
           <button
             className="btn clear-btn"
-            onClick={() => dispatch(clearCart())}
+            onClick={() => dispatch(openModal())}
           >
             Clear Cart
           </button>
